e8352f90f022ffe7e9fd6c209bbecf709392dc9a
SiposZoltan63 <siposz@kkszki.hu>
2025-01-31 13:24:06 +0100

    kesz0131

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace usercrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(61, 23);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 0;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(61, 79);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 1;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(370, 23);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 2;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(370, 168);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 3;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(370, 116);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 4;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(370, 67);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Jelszó";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Felhasználónév";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Regisztrál";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Belép";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(396, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Vezetéknév";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(396, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Keresztnév";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(405, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Jelszó";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(375, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Jelszó mégegyszer";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(581, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 251);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(705, 296);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Kilépés";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(61, 170);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 17);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Módosítás";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(62, 207);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(54, 17);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Törlés";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1001, 347);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using Org.BouncyCastle.Bcpg;
+
+namespace usercrud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ControlBox = false;
+            radioButton1.Checked = true;
+            showReg();
+            hideReg();
+            feltolt();
+            Update();
+        }
+
+        private Connect conn = new Connect();
+        public static int userId = 0;
+        private bool beleptet(string FirstName, string LastName, string pass)
+        {
+            conn.Connection.Open();
+
+            string sql = $"SELECT `Id` FROM `data` WHERE `FirstName` = '{FirstName}' and `LastName` = '{LastName}' AND `Password` = '{pass}'";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            bool van = dr.Read();
+
+            conn.Connection.Close();
+            return van;
+        }
+
+        private string regisztral(string FirstName, string LastName, string pass)
+        {
+            conn.Connection.Open();
+
+            string sql = $"INSERT INTO `data`(`FirstName`, `LastName`, `Password`, `CreatedTime`, `UpdateTime`) VALUES ('{FirstName}','{LastName}','{pass}','{DateTime.Now.ToString("yyyy-MM-dd")}','{DateTime.Now.ToString("yyyy-MM-dd")}')";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            var result = cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+            feltolt();
+
+            return result > 0 ? "Sikeres regisztráció" : "Sikertelen Regisztráció";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true && textBox11 == textBox10)
+            {
+                update(userId,textBox12.Text,textBox9.Text,textBox11.Text);
+                MessageBox.Show("Sikeres frissítés");
+                listBox1.Items.Clear();
+                feltolt();
+                hideReg();
+            }
+            else if (textBox11 == textBox10 && radioButton1.Checked == false)
+            {
+                MessageBox.Show(regisztral(textBox9.Text, textBox12.Text, textBox11.Text));
+
+            }
+            if (textBox11.Text == textBox10.Text)
+            {
+                MessageBox.Show(regisztral(textBox9.Text, textBox12.Text, textBox11.Text));
+                hideReg();
+            }
+            else
+            {
+
+                MessageBox.Show("A két jelszó nem egyezik");
+            }
+        }
+        private void regisztraltTag()
+        {
+            MessageBox.Show("Regisztrált tag.");
+        }
+
+        private void nemRegisztraltTag()
+        {
+            MessageBox.Show("Még nem regisztrált.");
+            showReg();
+            string[] darabol2 = textBox7.Text.Split(' ');
+            textBox9.Text = darabol2[0];
+            textBox12.Text = darabol2[1];
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            string[] darabol = textBox7.Text.Split(' ');
+
+            if (beleptet(darabol[0], darabol[1], textBox8.Text) == true)
+            {
+               regisztraltTag();
+            }
+            else
+            {
+                nemRegisztraltTag();
+            }
+        }
+
+        private void feltolt()
+        {
+            conn.Connection.Open();
+
+            string sql = $"SELECT `Id`,`LastName`,`FirstName`,`CreatedTime`,`UpdateTime` FROM `data`;";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            
+
+            while (dr.Read())
+            {
+                listBox1.Items.Add($"{dr.GetInt32(0)}. {dr.GetString(1)} {dr.GetString(2)} {dr.GetDateTime(3).ToString("yyyy-MM-dd")} {dr.GetDateTime(4).ToString("yyyy-MM-dd")}");
+            }
+
+            conn.Connection.Close();
+        }
+
+        private void hideReg()
+        {
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+
+            textBox9.Visible = false;
+            textBox12.Visible = false;
+            textBox11.Visible = false;
+            textBox10.Visible = false;
+
+            button3.Visible = false;
+        }
+        private void showReg()
+        {
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+
+            textBox9.Visible = true;
+            textBox12.Visible = true;
+            textBox11.Visible = true;
+            textBox10.Visible = true;
+
+            button3.Visible = true;
+        }
+
+        private void torol()
+        {
+            string id = listBox1.SelectedItem.ToString();
+            string[] idDarabol = id.Split('.');
+
+            conn.Connection.Open();
+
+            string sql = $"DELETE FROM `data` WHERE `Id` = '{idDarabol[0]}'";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            var result = cmd.ExecuteNonQuery();
+            conn.Connection.Close();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                torol();
+                listBox1.Items.Clear();
+                feltolt();
+            }
+            else
+            {
+                showReg();
+                string[] darabol = listBox1.SelectedItem.ToString().Split(' ');
+                textBox12.Text = darabol[1];
+                textBox9.Text = darabol[2];
+                string[] darabol2 = listBox1.SelectedItem.ToString().Split('.');
+                userId = int.Parse(darabol2[0].TrimEnd());
+            }
+        }
+        private void update(int id, string FirstName, string LastName, string Password)
+        {
+            conn.Connection.Open();
+
+            string sql = $"UPDATE 'data' SET `FirstName`= '{FirstName}',`LastName`='{LastName}',`Password`='{Password}' WHERE `Id`= '{id}' ;";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            var result = cmd.ExecuteNonQuery();
+
+            conn.Connection.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
